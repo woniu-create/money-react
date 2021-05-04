9cfd0c47ffcd94de3632159c85dc1cfd86ea6840
luyandan <2103029766@qq.com>
2021-05-04 22:11:58 +0800

    finished  TagsSection UI of Money.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import Tags from './views/Tag';
 import Money from "./views/Money";
 import Statistics from "./views/Statistics";
 import NoMatch from "./views/NoMatch";
+import styled from "styled-components";
 
-
+const AppWrapper =styled.div`
+  color: #333;
+`
 function App() {
     return (
+        <AppWrapper>
         <Router>
             <Switch>
                 <Route path="/tags">
@@ -25,6 +29,7 @@ function App() {
                 </Route>
             </Switch>
         </Router>
+        </AppWrapper>
     );
 }
 export default App;
