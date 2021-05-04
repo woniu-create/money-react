abb11611922ed2a486fbf22c206fe47d0653bf92
luyandan <2103029766@qq.com>
2021-05-04 22:30:43 +0800

    finshed All Money.tsx UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,11 @@ import styled from "styled-components";
 const TagsSection = styled.section`
   background: #fff;
   padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
   > ol {
     margin: 0 -12px;
     > li{
@@ -114,9 +119,14 @@ const NumberPadSection = styled.section`
   }
 `;
 
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`
+
 function Money() {
     return (
-        <Layout>
+        <MyLayout>
             <TagsSection>
                 <ol>
                     <li>衣</li>
@@ -157,7 +167,7 @@ function Money() {
                     <button className="dot">.</button>
                 </div>
             </NumberPadSection>
-        </Layout>
+        </MyLayout>
     );
 }
 export default Money;
