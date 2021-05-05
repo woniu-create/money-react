35dff9f7405c7799ccbb4d6d766ee9100b0152e4
luyandan <2103029766@qq.com>
2021-05-05 23:12:08 +0800

    将tag:string重构为tag:{id:number;name:string}

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -14,21 +14,21 @@ type Category = '-' | '+'
 
 function Money() {
   const [selected,setSelected] = useState({
-    tags: [] as string[],
+    tagIds: [] as number[],
     note: '',
     category: '-' as Category,
     amount: 0
   })
     return (
         <MyLayout>
-           {selected.tags.join(',')}
+           {selected.tagIds.join(',')}
            <hr/>
            {selected.note}
            <hr/>
            {selected.category}
            <hr/>
            {selected.amount}
-           <TagsSection value={selected.tags} onChange={(tags)=>setSelected({...selected,tags:tags})}/>
+           <TagsSection value={selected.tagIds} onChange={(tagIds)=>setSelected({...selected,tagIds:tagIds})}/>
            <NotesSection value={selected.note} onChange={(note)=>setSelected({...selected,note:note})}/>
            <CategorySection value={selected.category} onChange={(category)=>setSelected({...selected,category:category})}/>
            <NumberPadSection value={selected.amount} onChange={(amount)=>setSelected({...selected,amount:amount})} onOK = {()=>{}}/>
