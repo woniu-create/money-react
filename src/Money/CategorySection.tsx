9199306f38646747994a331ac453e9c79d3a9e62
luyandan <2103029766@qq.com>
2021-05-05 14:22:09 +0800

    finshed category of Money

--- src/Money/CategorySection.tsx
+++ src/Money/CategorySection.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React from "react";
+import React,{useState} from "react";
 
 const Wrapper= styled.section`
   font-size: 24px;
@@ -24,12 +24,14 @@ const Wrapper= styled.section`
     }
   }
 `;
-const CategorySection = () => {
+const CategorySection: React.FC = () => {
+const categoryMap = {'-': '支出','+': '收入'};
+const [categoryList] = useState<('+' | '-')[]>(['-','+']);
+const [category,setCategory] = useState('-');
     return (
      <Wrapper>
          <ul>
-             <li className="selected">支出</li>
-             <li>收入</li>
+           {categoryList.map(c=><li key={c} onClick={()=>{setCategory(c)}} className={category === c? 'selected' : ''}>{categoryMap[c]}</li>)}
          </ul>
      </Wrapper>
     )
