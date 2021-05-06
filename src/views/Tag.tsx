5176e43a613adc29ba529d893e43cd89d562c7ce
luyandan <2103029766@qq.com>
2021-05-06 23:30:14 +0800

    实现Tag点击回退功能

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useTags } from "../useTags";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import Layout from "components/Layout";
 import Icon from "components/Icon";
 import { Button } from "components/Button";
 import styled from "styled-components";
-import {Input}  from 'components/Input';
-import {Center} from 'components/Center';
-import {Space} from 'components/Space';
+import { Input } from "components/Input";
+import { Center } from "components/Center";
+import { Space } from "components/Space";
 
 const Topbar = styled.header`
   display: flex;
@@ -18,7 +18,7 @@ const Topbar = styled.header`
   background: white;
 `;
 const InputWrapper = styled.div`
-  background:white;
+  background: white;
   padding: 0 16px;
   margin-top: 8px;
 `;
@@ -26,30 +26,48 @@ type Params = {
   id: string;
 };
 const Tag: React.FC = (props) => {
-  const { findTag,updateTag,deleteTag } = useTags();
-  let { id:idString } = useParams<Params>();
+  const { findTag, updateTag, deleteTag } = useTags();
+  let { id: idString } = useParams<Params>();
   const tag = findTag(parseInt(idString));
-  const tagContent = (tag: {id: number;name: string}) => (
+  const tagContent = (tag: { id: number; name: string }) => (
     <div>
-    <InputWrapper>
-      <Input label="标签名" type="text" placeholder="标签名" value={tag.name} onChange={(e) => {updateTag(tag.id,{name: e.target.value})}}/>
-    </InputWrapper>
-    <Center>
-              <Space/>
-              <Space/>
-              <Space/>
-          <Button onClick={()=>{deleteTag(tag.id)}}>删除标签</Button>
-          </Center>
-</div>
-  )
+      <InputWrapper>
+        <Input
+          label="标签名"
+          type="text"
+          placeholder="标签名"
+          value={tag.name}
+          onChange={(e) => {
+            updateTag(tag.id, { name: e.target.value });
+          }}
+        />
+      </InputWrapper>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+        <Button
+          onClick={() => {
+            deleteTag(tag.id);
+          }}
+        >
+          删除标签
+        </Button>
+      </Center>
+    </div>
+  );
+  const history = useHistory()
+  const onClickBack = () => {
+   history.goBack()
+  };
   return (
     <Layout>
       <Topbar>
-        <Icon name="left" />
+        <Icon name="left" onClick={onClickBack} />
         <div>编辑标签</div>
         <Icon />
       </Topbar>
-      {tag? tagContent(tag): <Center>tag不存在</Center>}
+      {tag ? tagContent(tag) : <Center>tag不存在</Center>}
     </Layout>
   );
 };
