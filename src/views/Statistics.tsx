fb2e0df349fbad34d6d9a89d77dfab2fbb5a6afa
luyandan <2103029766@qq.com>
2021-05-07 20:57:32 +0800

    展示分组

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,8 +1,8 @@
 import Layout from "../components/Layout";
-import React,{useState} from "react";
+import React,{ReactNode,useState} from "react";
 import styled from 'styled-components';
 import {CategorySection} from "../Money/CategorySection";
-import {useRecords} from 'hooks/useRecords'
+import {RecordItem, useRecords} from 'hooks/useRecords'
 import {useTags} from 'hooks/useTags';
 import day from 'dayjs'
 
@@ -22,22 +22,48 @@ const Item = styled.div`
     color: #999;
   }
 `;
+const Header =styled.h3`
+  font-size:18px;
+  line-height: 20px;
+  padding: 10px 16px;
+`
 
 function Statistics() {
     const [category,setCategory] = useState <'-'|'+'>('-');
     const {records} = useRecords();
     const {getName} = useTags()
+    const hash: {[K: string]: RecordItem[]} = {}
+    const selectedRecords = records.filter(r => r.category === category);
+    selectedRecords.map(r=>{
+        const key = day(r.createAt).format('YYYY-MM-DD')
+        if(!(key in hash)){
+            hash[key] = []
+        }
+        hash[key].push(r)
+        // console.log(Object.keys(hash));
+        // console.log('--------------');
+        // console.log(Object.values(hash));
+    })
+    const array = Object.entries(hash).sort((a,b) => {
+        if(a[0] === b[0]) return 0;
+        if(a[0] > b[0]) return -1;
+        if(a[0] < b[0]) return 1;
+        return 0;
+    })
+    console.log(array);
+    
     return (
         <Layout>
             <CategoryWrapper>
             < CategorySection value={category} onChange={(value)=>setCategory(value)}/>
             </CategoryWrapper>
+            {array.map(([date,records]) => <div>
+                <Header>{date}</Header>
             <div>
-                {/* {JSON.stringify(records)} */}
                 {records.map(r => {
                 return <Item>
-                    <div className="tags">
-                        {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
+                    <div className="tags oneLine">
+                        {r.tagIds.map(tagId => <span key={tagId}>{getName(tagId)}</span>)}
                     </div>
                     {r.note && <div className="note">
                        {r.note}    
@@ -47,6 +73,7 @@ function Statistics() {
                 </Item>
                 })}
             </div>
+            </div>)}
         </Layout>
     );
 }
