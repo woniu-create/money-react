fa62c8f386eff4bc54a11abffab4fd5c72a9e424
luyandan <2103029766@qq.com>
2021-05-07 10:55:23 +0800

    money页面数据存到localStorage

---
+++ src/hooks/useRecords.tsx
@@ -0,0 +1,26 @@
+import {useEffect,useState} from 'react';
+import {useUpdate} from './useUpdate';
+
+type RecordItem = {
+    tagIds: number[]
+    note: string
+    category: '+' | '-'
+    amount: number
+    createAt: string
+}
+type newRecordItem = Omit<RecordItem,'createAt'>
+const useRecords = () => {
+    const [records,setRecords] = useState<RecordItem[]>([]);
+    useEffect (()=>{
+     setRecords(JSON.parse(window.localStorage.getItem('records') || '[]'))
+    },[]);
+    useUpdate(()=>{
+      window.localStorage.setItem('records',JSON.stringify(records))
+    },[records]);
+    const addRecord = (newRecord: newRecordItem) => {
+        const record = {...newRecord,createAt: (new Date()).toISOString()}
+        setRecords([...records,record]);
+    }
+    return {records,addRecord}
+}
+export {useRecords}
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,7 @@ import {TagsSection} from "../Money/TagsSection";
 import {NotesSection} from "../Money/NotesSection";
 import {CategorySection} from "../Money/CategorySection";
 import {NumberPadSection} from "../Money/NumberPadSection";
+import {useRecords} from "hooks/useRecords"
 
 const MyLayout = styled(Layout)`
   display: flex;
@@ -12,26 +13,28 @@ const MyLayout = styled(Layout)`
 `
 type Category = '-' | '+'
 
+const defaultFormDate = {
+  tagIds: [] as number[],
+  note: '',
+  category: '-' as Category,
+  amount: 0
+}
+
 function Money() {
-  const [selected,setSelected] = useState({
-    tagIds: [] as number[],
-    note: '',
-    category: '-' as Category,
-    amount: 0
-  })
+  const [selected,setSelected] = useState(defaultFormDate)
+  const {records,addRecord} = useRecords();
+  const submit = () => {
+    addRecord(selected);
+    alert('保存成功');
+    setSelected(defaultFormDate) //保存成功后置空
+  }
     return (
         <MyLayout>
-           {selected.tagIds.join(',')}
-           <hr/>
-           {selected.note}
-           <hr/>
-           {selected.category}
-           <hr/>
-           {selected.amount}
+            {JSON.stringify(selected)}
            <TagsSection value={selected.tagIds} onChange={(tagIds)=>setSelected({...selected,tagIds:tagIds})}/>
            <NotesSection value={selected.note} onChange={(note)=>setSelected({...selected,note:note})}/>
            <CategorySection value={selected.category} onChange={(category)=>setSelected({...selected,category:category})}/>
-           <NumberPadSection value={selected.amount} onChange={(amount)=>setSelected({...selected,amount:amount})} onOK = {()=>{}}/>
+           <NumberPadSection value={selected.amount} onChange={(amount)=>setSelected({...selected,amount:amount})} onOK = {submit}/>
         </MyLayout>
     );
 }
